0061d04eda27099175bb9aa8e6c346c29ee06125
Sagar <sagarghosh4000@gmail.com>
2023-03-04 23:47:44 +0600

    all-done

--- js/script.js
+++ js/script.js
@@ -1,68 +1,68 @@
 // fetch data functionality
 
 const fetchData = () => {
-    const URL = "https://openapi.programming-hero.com/api/ai/tools";
-    try {
-        fetch(URL)
-            .then((res) => res.json())
-            .then((data) => displayDataFunc(data.data.tools));
-    } catch (err) {
-        console.log(err);
-    }
+  const URL = "https://openapi.programming-hero.com/api/ai/tools";
+  try {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => displayDataFunc(data.data.tools));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-// function determines how the data will be displayed on the display.
+// Functions determine how the data appears on the display.
 
 const displayDataFunc = (allToolsData) => {
-    const mainContainer = document.getElementById("main-container");
-    const showAllBtn = document.getElementById("showAll-btn");
-    const sortByDateBtn = document.getElementById("sortByDateBtn");
+  const mainContainer = document.getElementById("main-container");
+  const showAllBtn = document.getElementById("showAll-btn");
+  const sortByDateBtn = document.getElementById("sortByDateBtn");
 
-    // The following function works on how the data will be shown on the display in an ascending manner through the date.
+  // The function works on how to display data in an ascending manner by date.
 
-    sortByDateBtn.addEventListener("click", () => {
-        sortByDateBtn.style.cursor = "not-allowed";
-        const compareDates = (dateOne, dateTwo) => {
-            const firstDate = new Date(
-                dateOne.published_in.split("/").reverse().join("-")
-            );
-            const secondDate = new Date(
-                dateTwo.published_in.split("/").reverse().join("-")
-            );
+  sortByDateBtn.addEventListener("click", () => {
+    sortByDateBtn.style.cursor = "not-allowed";
+    const compareDates = (dateOne, dateTwo) => {
+      const firstDate = new Date(
+        dateOne.published_in.split("/").reverse().join("-")
+      );
+      const secondDate = new Date(
+        dateTwo.published_in.split("/").reverse().join("-")
+      );
 
-            return firstDate - secondDate;
-        };
-        allToolsData.sort(compareDates);
-        displayDataFunc(allToolsData);
+      return firstDate - secondDate;
+    };
+    allToolsData.sort(compareDates);
+    displayDataFunc(allToolsData);
+  });
+
+  // How to first show 6 data and then how to make the button visible through condition
+
+  const firstSixItems = allToolsData.slice(0, 6);
+  if (allToolsData.length > firstSixItems.length) {
+    showAllBtn.style.display = "block";
+    showAllBtn.addEventListener("click", () => {
+      mainContainer.innerHTML = "";
+      displayShowData(allToolsData);
+
+      showAllBtn.style.display = "none";
     });
+  } else {
+    showAllBtn.style.display = "none";
+  }
 
-    // How to show 6 data first and then how to show all the data by making the button visible through the condition.
-
-    const firstSixItems = allToolsData.slice(0, 6);
-    if (allToolsData.length > firstSixItems.length) {
-        showAllBtn.style.display = "block";
-        showAllBtn.addEventListener("click", () => {
-            mainContainer.innerHTML = "";
-            displayShowData(allToolsData);
-
-            showAllBtn.style.display = "none";
-        });
-    } else {
-        showAllBtn.style.display = "none";
-    }
-
-    displayShowData(firstSixItems);
+  displayShowData(firstSixItems);
 };
 
-// The work of the following function is only to display the data.
+// Function to display only the data.
 
 const displayShowData = (allTools) => {
-    const mainContainer = document.getElementById("main-container");
-    mainContainer.innerHTML = "";
-    allTools.forEach((singleTool) => {
-        const { image, name, features, published_in, id } = singleTool;
+  const mainContainer = document.getElementById("main-container");
+  mainContainer.innerHTML = "";
+  allTools.forEach((singleTool) => {
+    const { image, name, features, published_in, id } = singleTool;
 
-        mainContainer.innerHTML += `
+    mainContainer.innerHTML += `
           
           <div class="card glass p-4 mx-8">
           <figure>
@@ -72,8 +72,8 @@ const displayShowData = (allTools) => {
             <h2 class="mt-2 text-xl font-semibold">Features:</h2>
             <ul class="list-decimal">
              ${features
-                .map((feature) => `<li class="ml-4">${feature}</li>`)
-                .join("")}
+        .map((feature) => `<li class="ml-4">${feature}</li>`)
+        .join("")}
             </ul>
       
           </div>
@@ -89,44 +89,44 @@ const displayShowData = (allTools) => {
         </div>
        
           `;
-    });
+  });
 };
 
-// The work of the following function is to fetch the data through dynamic id
+// The function's job is to fetch data by dynamic ID
 
 const toolInfo = (id) => {
-    const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-    try {
-        fetch(URL)
-            .then((res) => res.json())
-            .then((data) => displayToolInfo(data.data));
-    } catch (err) {
-        console.log(err);
-    }
+  const URL = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  try {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => displayToolInfo(data.data));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-// function is to display the data of dynamic id through modal.
+// The function is to display the data of dynamic id through a modal.
 
 const displayToolInfo = (singleToolInfo) => {
-    const {
-        description,
-        pricing,
-        features,
-        integrations,
-        image_link,
-        input_output_examples,
-        accuracy,
-    } = singleToolInfo;
+  const {
+    description,
+    pricing,
+    features,
+    integrations,
+    image_link,
+    input_output_examples,
+    accuracy,
+  } = singleToolInfo;
 
-    const featureArr = [];
-    for (const feature in features) {
-        const values = Object.values(features[feature]);
-        featureArr.push(values[0]);
-    }
+  const featureArr = [];
+  for (const feature in features) {
+    const values = Object.values(features[feature]);
+    featureArr.push(values[0]);
+  }
 
-    const modalInfo = document.getElementById("modal-info");
-    modalInfo.innerHTML = "";
-    modalInfo.innerHTML += `
+  const modalInfo = document.getElementById("modal-info");
+  modalInfo.innerHTML = "";
+  modalInfo.innerHTML += `
     
     <div class="shadow-sm p-2 rounded-md bg-red-100">
     <p class="font-semibold text-lg mb-2">
@@ -137,19 +137,19 @@ const displayToolInfo = (singleToolInfo) => {
         class="flex flex-col items-center justify-center bg-white p-2 rounded-md text-green-500 font-bold"
       >
         <span>${pricing !== null ? pricing[0].price : "Free Of Cost"} </span> ${pricing !== null ? "/" + pricing[0].plan : "/Basic"
-        }
+    }
       </div>
       <div
         class="flex flex-col items-center justify-center bg-white p-2 rounded-md text-orange-500 font-bold"
       >
       <span>${pricing !== null ? pricing[1].price : "Free Of Cost"} </span> ${pricing !== null ? "/" + pricing[1].plan : "/Pro"
-        }
+    }
       </div>
       <div
         class="flex flex-col items-center justify-center bg-white p-2 rounded-md text-blue-500 font-bold"
       >
       <span>${pricing !== null ? pricing[2].price : "Free Of Cost"} </span> ${pricing !== null ? "/" + pricing[2].plan : "/Enterprise"
-        }
+    }
       </div>
     </div>
     <div class="flex gap-4 justify-between px-2">
@@ -157,20 +157,20 @@ const displayToolInfo = (singleToolInfo) => {
         <h2 class="font-semibold">Features:</h2>
         <ul class="list-disc ml-4">
             ${featureArr
-            ? featureArr.map((feature) => `<li>${feature}</li>`).join("")
-            : `<span class="ml-[-15px] font-medium text-yellow-500">No Data Found</span>`
-        }
+      ? featureArr.map((feature) => `<li>${feature}</li>`).join("")
+      : `<span class="ml-[-15px] font-medium text-yellow-500">No Data Found</span>`
+    }
         </ul>
       </div>
       <div>
         <h2 class="font-semibold">Integrations:</h2>
         <ul class="list-disc ml-4">
         ${integrations
-            ? integrations
-                .map((integration) => `<li>${integration}</li>`)
-                .join("")
-            : `<span class="ml-[-15px] font-medium text-yellow-500">No Data Found</span>`
-        }
+      ? integrations
+        .map((integration) => `<li>${integration}</li>`)
+        .join("")
+      : `<span class="ml-[-15px] font-medium text-yellow-500">No Data Found</span>`
+    }
         
         </ul>
       </div>
@@ -183,23 +183,23 @@ const displayToolInfo = (singleToolInfo) => {
       alt=""
     />
     <h3 class="text-center text-lg font-bold mt-2">${input_output_examples
-            ? input_output_examples[0].input
-            : "No Question Available"
-        } </h3>
+      ? input_output_examples[0].input
+      : "No Question Available"
+    } </h3>
     <p class="text-center mt-1 ">
       ${input_output_examples
-            ? input_output_examples[0].output
-            : "No! Not Yet! Take a break!!!"
-        }
+      ? input_output_examples[0].output
+      : "No! Not Yet! Take a break!!!"
+    }
     </p>
     ${accuracy.score
-            ? `
+      ? `
     <p class="bg-red-500 p-2 rounded-md w-fit text-white text-base absolute top-0 right-0 mr-4 mt-4 accuracy-text"><span> ${accuracy.score * 100 + "%"
-            } </span>Accuracy</p>
+      } </span>Accuracy</p>
     
     `
-            : `<p class="bg-red-500 p-2 rounded-md w-fit text-white text-base absolute top-0 right-0 mr-4 mt-4 hidden"><span></span>Accuracy</p>`
-        }
+      : `<p class="bg-red-500 p-2 rounded-md w-fit text-white text-base absolute top-0 right-0 mr-4 mt-4 hidden"><span></span>Accuracy</p>`
+    }
     
   </div>
     
@@ -208,11 +208,11 @@ const displayToolInfo = (singleToolInfo) => {
 
 fetchData();
 
-//The function is to display the website loading animation
+//The function is to display the website loading animation.
 
 window.addEventListener("load", () => {
-    const loader = document.getElementById("loader-section");
-    setTimeout(function () {
-        loader.style.display = "none";
-    }, 1000);
+  const loader = document.getElementById("loader-section");
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 1000);
 });
