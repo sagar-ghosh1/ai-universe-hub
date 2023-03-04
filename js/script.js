375722fd73be22bc49bb9592689d7bd512e5d346
Sagar <sagarghosh4000@gmail.com>
2023-03-04 22:20:45 +0600

    function-determines

--- js/script.js
+++ js/script.js
@@ -11,3 +11,46 @@ const fetchData = () => {
     }
 };
 
+// function determines how the data will be displayed on the display.
+
+const displayDataFunc = (allToolsData) => {
+    const mainContainer = document.getElementById("main-container");
+    const showAllBtn = document.getElementById("showAll-btn");
+    const sortByDateBtn = document.getElementById("sortByDateBtn");
+
+    // The following function works on how the data will be shown on the display in an ascending manner through the date.
+
+    sortByDateBtn.addEventListener("click", () => {
+        sortByDateBtn.style.cursor = "not-allowed";
+        const compareDates = (dateOne, dateTwo) => {
+            const firstDate = new Date(
+                dateOne.published_in.split("/").reverse().join("-")
+            );
+            const secondDate = new Date(
+                dateTwo.published_in.split("/").reverse().join("-")
+            );
+
+            return firstDate - secondDate;
+        };
+        allToolsData.sort(compareDates);
+        displayDataFunc(allToolsData);
+    });
+
+    // How to show 6 data first and then how to show all the data by making the button visible through the condition.
+
+    const firstSixItems = allToolsData.slice(0, 6);
+    if (allToolsData.length > firstSixItems.length) {
+        showAllBtn.style.display = "block";
+        showAllBtn.addEventListener("click", () => {
+            mainContainer.innerHTML = "";
+            displayShowData(allToolsData);
+
+            showAllBtn.style.display = "none";
+        });
+    } else {
+        showAllBtn.style.display = "none";
+    }
+
+    displayShowData(firstSixItems);
+};
+
